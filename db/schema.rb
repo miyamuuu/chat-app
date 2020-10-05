2820ed23624e1c5b5ccbbe5076e61c3e318e042e
miyamuuu <miyamuuu386@gmail.com>
2020-10-05 14:57:08 +0900

    画面遷移の実装

--- db/schema.rb
+++ db/schema.rb
@@ -18,9 +18,12 @@ ActiveRecord::Schema.define(version: 2020_10_05_020851) do
   end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
+    t.bigint "room_id"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_rooms_on_room_id"
+    t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -36,4 +39,6 @@ ActiveRecord::Schema.define(version: 2020_10_05_020851) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "rooms", "rooms"
+  add_foreign_key "rooms", "users"
 end
